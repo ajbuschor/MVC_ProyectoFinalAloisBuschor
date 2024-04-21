6af0133520c9be23730eae62771b6baeac482d07
ajbuschor <jbuschor03@gmail.com>
2024-04-21 11:50:23 -0600

    Agregar archivos de proyecto.

---
+++ Models/ACME/EmpresaEntidad.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models.ACME
+{
+    public class EmpresaEntidad
+    {
+        [Range(0,int.MaxValue,ErrorMessage ="Debe seleccionar una empresa.")]
+        [Display(Name ="Codigo")]
+        public int IDEmpresa {  get; set; }
+        [Required(ErrorMessage ="Debe seleccionar un tipo de empresa.")]
+        [Range(1,int.MaxValue,ErrorMessage ="Debe seleccionar un tipo de empresa.")]
+        [Display(Name ="Tipo Empresa")]
+        public int? IDTipoEmpresa {  get; set; }
+        [Required(ErrorMessage ="El nombre de la empresa es obligatorio.")]
+        [Display(Name ="Nombre empresa")]
+        public string Empresa { get; set; }=string.Empty;
+        [Required(ErrorMessage ="La dirrecion de la empresa es obligatoria.")]
+        [Display(Name ="Direccion ")]
+        public string Direccion {  get; set; }=string.Empty ;
+        [Required(ErrorMessage ="El RUC de la empresa es obligatorio")]
+        [Display(Name ="RUC")]
+        public string RUC {  get; set; }= string.Empty ;
+        [Required(ErrorMessage = "Debe ingresa la fecha de creacion")]
+        [Display(Name = "Fecha creacion")]
+        public DateTime FechaCreacion { get; set; }=DateTime.Now ;
+        [Required(ErrorMessage = "Debe ingresar el presupuesto")]
+        [Display(Name = "Presupuesto")]
+        public decimal Presupuesto { get; set; }
+        public bool Activo { get; set; }=true ;
+        public TipoEmpresaEntidad? TipoEmpresaEntidad { get; set; } 
+
+    }
+}
---
+++ Services/ACME/EmpresaService.cs
@@ -0,0 +1,80 @@
+﻿using Models.ACME;
+using DataAccess.ACME;
+
+namespace Services.ACME
+{
+    public class EmpresaService
+    {
+        public bool Crear(EmpresaEntidad empresaEntidad)
+        {
+            EmpresaDA empresaDA= new EmpresaDA();
+            try
+            {
+                empresaDA.Insertar(empresaEntidad);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Actualizar(EmpresaEntidad empresaEntidad)
+        {
+            EmpresaDA empresaDA = new EmpresaDA();
+            try
+            {
+                empresaDA.Modificar(empresaEntidad);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public EmpresaEntidad BuscarxID(int IDEmpresa)
+        {
+            EmpresaDA? empresaDA=new EmpresaDA();
+
+            try
+            {
+                return empresaDA.BuscarID(IDEmpresa);
+
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public List<EmpresaEntidad> Listar()
+        {
+            EmpresaDA? empresaDA = new EmpresaDA();
+
+            try
+            {
+                return empresaDA.LIstar();
+
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public bool Eliminar(EmpresaEntidad empresaEntidad)
+        {
+            EmpresaDA empresaDA = new EmpresaDA();
+            try
+            {
+                empresaDA.Anular(empresaEntidad);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
